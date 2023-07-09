9e200c8a6a4defdf989d4de962e59085d8b53c31
Ravi Lour <ravijaat2917@gmail.com>
2023-07-09 19:33:09 +0530

    docker

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import morgan from 'morgan';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import UserRoutes from './Routes/user.Routes.js';
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -15,8 +17,15 @@ app.use(cors());
 
 connectDB(process.env.DATABASE_URI);
 
-app.use('/user' , UserRoutes)
+app.use('/user', UserRoutes);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+    
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
